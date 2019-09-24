6a988b7b64ba0ed1e3cf41d1e43d558f44bc2a01
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2019-09-24 09:08:23 +0100

    tests: Add tests for tar_element includes

--- tests/tar_element.py
+++ tests/tar_element.py
@@ -1,30 +1,100 @@
+# Pylint doesn't play well with fixtures and dependency injection from pytest
+# pylint: disable=redefined-outer-name
+
 import os
-import pytest
 import tarfile
 
-from tests.testutils import cli_integration as cli
-from tests.testutils.integration import assert_contains
+import pytest
+
+from .testutils.integration import assert_contains
+from .testutils.runcli import cli_integration as cli  # pylint: disable=unused-import
+
+pytestmark = pytest.mark.integration
 
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "project"
 )
 
+
+BLOCKSIZE = 65536
+
+
 @pytest.mark.datafiles(DATA_DIR)
 def test_tar_build(cli, datafiles):
     project = str(datafiles)
     checkout_dir = os.path.join(cli.directory, 'tar_checkout')
     tarpath = os.path.join(checkout_dir, 'hello.tar.gz')
+    element_name = 'tar/tar-test.bst'
 
-    result = cli.run(project=project, args=['build', 'tar-test.bst'])
+    result = cli.run(project=project, args=['build', element_name])
     result.assert_success()
 
-    result = cli.run(project=project, args=['checkout', 'tar-test.bst',
-        checkout_dir])
+    result = cli.run(project=project, args=['checkout', element_name, checkout_dir])
     result.assert_success()
 
     assert_contains(checkout_dir, ["/hello.tar.gz"])
 
     tar_hello = tarfile.open(tarpath)
-    contents = tar_hello.getnames()
-    assert contents == ['hello.c']
+    contents = set(tar_hello.getnames())
+    assert contents == {'hello.c', 'Makefile', 'series', 'test'}
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_tar_include(cli, datafiles):
+    project = str(datafiles)
+    checkout_dir = os.path.join(cli.directory, 'tar_checkout')
+    tarpath = os.path.join(checkout_dir, 'hello.tar.gz')
+    element_name = 'tar/tar-test-include.bst'
+
+    result = cli.run(project=project, args=['build', element_name])
+    result.assert_success()
+
+    result = cli.run(project=project, args=['checkout', element_name, checkout_dir])
+    result.assert_success()
+
+    assert_contains(checkout_dir, ["/hello.tar.gz"])
+
+    tar_hello = tarfile.open(tarpath)
+    contents = set(tar_hello.getnames())
+    assert contents == {'hello.c', 'Makefile'}
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_tar_exclude(cli, datafiles):
+    project = str(datafiles)
+    checkout_dir = os.path.join(cli.directory, 'tar_checkout')
+    tarpath = os.path.join(checkout_dir, 'hello.tar.gz')
+    element_name = 'tar/tar-test-exclude.bst'
+
+    result = cli.run(project=project, args=['build', element_name])
+    result.assert_success()
+
+    result = cli.run(project=project, args=['checkout', element_name, checkout_dir])
+    result.assert_success()
+
+    assert_contains(checkout_dir, ["/hello.tar.gz"])
+
+    tar_hello = tarfile.open(tarpath)
+    contents = set(tar_hello.getnames())
+    assert contents == {'hello.c', 'Makefile'}
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_tar_orphans(cli, datafiles):
+    project = str(datafiles)
+    checkout_dir = os.path.join(cli.directory, 'tar_checkout')
+    tarpath = os.path.join(checkout_dir, 'hello.tar.gz')
+    element_name = 'tar/tar-test-orphans.bst'
+
+    result = cli.run(project=project, args=['build', element_name])
+    result.assert_success()
+
+    result = cli.run(project=project, args=['checkout', element_name, checkout_dir])
+    result.assert_success()
+
+    assert_contains(checkout_dir, ["/hello.tar.gz"])
+
+    tar_hello = tarfile.open(tarpath)
+    contents = set(tar_hello.getnames())
+    assert contents == {'hello.c'}
